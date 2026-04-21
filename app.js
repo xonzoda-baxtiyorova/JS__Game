84c3d9118a4fa04ac389942076a0ab2204195ecb
Abdulaziz Sadullayev <sadullayevabdulaziz2009@gmail.com>
2026-04-21 16:28:19 +0500

    added some code

---
+++ app.js
@@ -0,0 +1,11 @@
+// JS Codes
+
+const button = document.querySelector(".btn");
+const first_player = document.querySelector('.first_player')
+const second_player = document.querySelector('.second_player')
+
+button.addEventListener("click", () => {
+    const first_random_number = Math.round(Math.random() * 2);
+    const second_random_number = Math.round(Math.random() * 2);
+
+});
